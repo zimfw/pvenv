81bce67a8baee4aa54aa5d0b1d86b9cd705d3146
Eric Nielsen <eric@amalgamar.com.br>
2021-03-20 09:53:16 -0500

    Don't depend on sha1sum anymore
    Use python3 instead of sha1sum, xxd, base64, tr and head.
    Fixes #1

--- init.zsh
+++ init.zsh
@@ -1,13 +1,5 @@
 # Prefer initializing with this script if your plugin framework supports the prezto modules format.
-
-# sha1sum is used to compute the hash for the venv suffix.
-if ! whence sha1sum >/dev/null; then
-  if (( ${+commands[shasum]} )); then
-    alias sha1sum='shasum -a1'
-  elif (( ${+commands[openssl]} )); then
-    alias sha1sum='openssl sha1'
-  else
-    print -u2 -PR "%F{red}${0}: sha1sum, shasum or openssl required.%f"
-    return 1
-  fi
+if ! whence python3 >/dev/null; then
+  print -u2 -PR "%F{red}${0:A}: python3 required.%f"
+  return 1
 fi
