114f0c5833df8a0471214d39d5963d2fa521ecde
Eric Nielsen <eric@amalgamar.com.br>
2021-03-23 17:12:57 -0500

    Remove oh-my-zsh installation instructions
    as making the module also compatible with oh-my-zsh is bringing extra
    complexity. We just want to maintain modules using the prezto modules
    format.

--- init.zsh
+++
@@ -1,5 +0,0 @@
-# Prefer initializing with this script if your plugin framework supports the prezto modules format.
-if ! whence python3 >/dev/null; then
-  print -u2 -PR "%F{red}${0:A}: python3 required.%f"
-  return 1
-fi
