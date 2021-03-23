1df92b3e647cd82150b416e21cd476ed1ce7f811
Eric Nielsen <eric@amalgamar.com.br>
2021-03-23 17:22:18 -0500

    Remove pvenv.plugin.zsh
    as it was intented to be used only by oh-my-zsh and frameworks not
    compatible with the prezto module format.

--- pvenv.plugin.zsh
+++
@@ -1,5 +0,0 @@
-# Boilerplate code for frameworks that don't support the prezto modules format.
-local -r pdir=${0:A:h}
-if (( ! ${fpath[(I)${pdir}/functions]} )) fpath+=(${pdir}/functions)
-autoload -Uz pvenv
-source ${pdir}/init.zsh
